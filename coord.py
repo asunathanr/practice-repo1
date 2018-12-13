0e43d3d9c893a7b816703b0e421f6168b23dd873
asunathanr <44170865+asunathanr@users.noreply.github.com>
2018-12-12 22:59:33 -0600

    Added hash and eq comparison

--- coord.py
+++ coord.py
@@ -1,6 +1,21 @@
 class Coord:
     x = 0
     y = 0
-    def __init__(self,x,y):
+
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def __eq__(self, other):
+        """
+        :param other: Another Coord object
+        :return: If self is equal to other and other is a Coord
+        """
+        return isinstance(other, self.__class__) and self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        """
+        For putting coords in sets or any situation a coord would need to be hashed
+        :return: Hash of a Coord object
+        """
+        return hash((self.x, self.y))
