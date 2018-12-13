597f9c4fa2d98350e3933f7babd1dacd013bc7a0
asunathanr <44170865+asunathanr@users.noreply.github.com>
2018-12-13 11:10:42 -0600

    Added more unit tests

--- map_grid_test.py
+++ map_grid_test.py
@@ -1,7 +1,11 @@
 import unittest
-
 from MapGrid import *
 from coord import Coord
+
+
+# File: map_grid_test.py
+# Authors: Jason Cassimus, Eric Day, Nathan Robertson
+# Purpose: Test the MapGrid class.
 
 
 class GridTest(unittest.TestCase):
@@ -17,6 +21,20 @@ class GridTest(unittest.TestCase):
         self.assertTrue(self.grid.is_adjacent(Coord(0, 0), Coord(0, 1)))
         self.assertTrue(self.grid.is_adjacent(Coord(1, 1), Coord(0, 1)))
 
-    def test_adjacent(self):
-        neighbors = self.grid.adjacent(Coord(1, 1))
-        self.assertEqual({Coord(1, 2), Coord(0, 1), Coord(2, 1), Coord(1, 0)}, set(neighbors))
+    def test_neighbors(self):
+        neighbors = self.grid.neighbors(Coord(1, 1))
+        expected = {Coord(1, 2), Coord(0, 1), Coord(2, 1), Coord(1, 0)}
+        self.assertEqual(expected, set(neighbors))
+
+    def test_out_of_bound_neighbors(self):
+        neighbors = self.grid.neighbors(Coord(0, -1))
+        self.assertEqual(set(), set(neighbors), "Expected coords not equivalent to actual coords.")
+
+
+class ObstacleGridTest(unittest.TestCase):
+    def setUp(self):
+        self.grid = MapGrid(3, 3, [])
+
+    def test_add_obstacle(self):
+        self.grid.insert_obstacle(Coord(1, 1))
+        self.assertEqual([Coord(1, 1)], self.grid.obstacles())
